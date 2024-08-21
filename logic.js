d15229384f2faca76ce760fdb1d58751d37aa20d
Suchit Bindra <95643760+suchitbindra@users.noreply.github.com>
2024-08-21 21:22:41 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,19 @@
+
+
+
+
+function addrem(){
+    
+let ul = document.querySelector('ul');
+var li = document.querySelectorAll('li');
+
+li.forEach(el => {
+
+    el.addEventListener('click', function(){
+    ul.querySelector('.active').classList.remove('active');
+     
+         el.classList.add('active');
+     }); 			
+});
+
+}
